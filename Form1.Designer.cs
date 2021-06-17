012fc14e96e3b0499d94ddce175afd93eb62119d
Chris Bartlett <79030655+Chris-Progs@users.noreply.github.com>
2021-06-17 15:45:04 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Scripting2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RedLEDButton = new System.Windows.Forms.Button();
+            this.OutsideTempBox = new System.Windows.Forms.TextBox();
+            this.InsideTempBox = new System.Windows.Forms.TextBox();
+            this.InsideHumidityBox = new System.Windows.Forms.TextBox();
+            this.OutsideTempLabel = new System.Windows.Forms.Label();
+            this.InsideTempLabel = new System.Windows.Forms.Label();
+            this.InsideHumidityLabel = new System.Windows.Forms.Label();
+            this.TempComboBox = new System.Windows.Forms.ComboBox();
+            this.TempMaxButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RedLEDButton
+            // 
+            this.RedLEDButton.BackColor = System.Drawing.Color.Red;
+            this.RedLEDButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RedLEDButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.RedLEDButton.Location = new System.Drawing.Point(27, 270);
+            this.RedLEDButton.Name = "RedLEDButton";
+            this.RedLEDButton.Size = new System.Drawing.Size(308, 54);
+            this.RedLEDButton.TabIndex = 2;
+            this.RedLEDButton.Text = "Red LED Toggle";
+            this.RedLEDButton.UseVisualStyleBackColor = false;
+            this.RedLEDButton.Click += new System.EventHandler(this.RedLEDButton_Click);
+            // 
+            // OutsideTempBox
+            // 
+            this.OutsideTempBox.Location = new System.Drawing.Point(152, 51);
+            this.OutsideTempBox.Name = "OutsideTempBox";
+            this.OutsideTempBox.Size = new System.Drawing.Size(122, 20);
+            this.OutsideTempBox.TabIndex = 3;
+            // 
+            // InsideTempBox
+            // 
+            this.InsideTempBox.Location = new System.Drawing.Point(152, 80);
+            this.InsideTempBox.Name = "InsideTempBox";
+            this.InsideTempBox.Size = new System.Drawing.Size(122, 20);
+            this.InsideTempBox.TabIndex = 4;
+            // 
+            // InsideHumidityBox
+            // 
+            this.InsideHumidityBox.Location = new System.Drawing.Point(152, 113);
+            this.InsideHumidityBox.Name = "InsideHumidityBox";
+            this.InsideHumidityBox.Size = new System.Drawing.Size(122, 20);
+            this.InsideHumidityBox.TabIndex = 5;
+            // 
+            // OutsideTempLabel
+            // 
+            this.OutsideTempLabel.AutoSize = true;
+            this.OutsideTempLabel.Location = new System.Drawing.Point(24, 54);
+            this.OutsideTempLabel.Name = "OutsideTempLabel";
+            this.OutsideTempLabel.Size = new System.Drawing.Size(109, 13);
+            this.OutsideTempLabel.TabIndex = 6;
+            this.OutsideTempLabel.Text = "Outside Temp (LM35)";
+            // 
+            // InsideTempLabel
+            // 
+            this.InsideTempLabel.AutoSize = true;
+            this.InsideTempLabel.Location = new System.Drawing.Point(24, 83);
+            this.InsideTempLabel.Name = "InsideTempLabel";
+            this.InsideTempLabel.Size = new System.Drawing.Size(109, 13);
+            this.InsideTempLabel.TabIndex = 7;
+            this.InsideTempLabel.Text = "Inside Temp (DHT11)";
+            // 
+            // InsideHumidityLabel
+            // 
+            this.InsideHumidityLabel.AutoSize = true;
+            this.InsideHumidityLabel.Location = new System.Drawing.Point(24, 116);
+            this.InsideHumidityLabel.Name = "InsideHumidityLabel";
+            this.InsideHumidityLabel.Size = new System.Drawing.Size(122, 13);
+            this.InsideHumidityLabel.TabIndex = 8;
+            this.InsideHumidityLabel.Text = "Inside Humidity (DHT11)";
+            // 
+            // TempComboBox
+            // 
+            this.TempComboBox.FormattingEnabled = true;
+            this.TempComboBox.Items.AddRange(new object[] {
+            "32",
+            "33",
+            "34"});
+            this.TempComboBox.Location = new System.Drawing.Point(160, 164);
+            this.TempComboBox.Name = "TempComboBox";
+            this.TempComboBox.Size = new System.Drawing.Size(148, 21);
+            this.TempComboBox.TabIndex = 9;
+            // 
+            // TempMaxButton
+            // 
+            this.TempMaxButton.Location = new System.Drawing.Point(27, 164);
+            this.TempMaxButton.Name = "TempMaxButton";
+            this.TempMaxButton.Size = new System.Drawing.Size(127, 21);
+            this.TempMaxButton.TabIndex = 10;
+            this.TempMaxButton.Text = "Temp Max";
+            this.TempMaxButton.UseVisualStyleBackColor = true;
+            this.TempMaxButton.Click += new System.EventHandler(this.TempMaxButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Turn Rotation AO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 371);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TempMaxButton);
+            this.Controls.Add(this.TempComboBox);
+            this.Controls.Add(this.InsideHumidityLabel);
+            this.Controls.Add(this.InsideTempLabel);
+            this.Controls.Add(this.OutsideTempLabel);
+            this.Controls.Add(this.InsideHumidityBox);
+            this.Controls.Add(this.InsideTempBox);
+            this.Controls.Add(this.OutsideTempBox);
+            this.Controls.Add(this.RedLEDButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button RedLEDButton;
+        private System.Windows.Forms.TextBox OutsideTempBox;
+        private System.Windows.Forms.TextBox InsideTempBox;
+        private System.Windows.Forms.TextBox InsideHumidityBox;
+        private System.Windows.Forms.Label OutsideTempLabel;
+        private System.Windows.Forms.Label InsideTempLabel;
+        private System.Windows.Forms.Label InsideHumidityLabel;
+        private System.Windows.Forms.ComboBox TempComboBox;
+        private System.Windows.Forms.Button TempMaxButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿// AT2.1 | Scripting
+// Arduino & Winform home climate control system prototype
+// Semester 2, Term 2, 2020
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace Scripting2._1
+{
+    public partial class Form1 : Form
+    {
+        public SerialPort myPort = new SerialPort();
+        bool redLightStatus = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+        // Method to initialise and setup serial port which connects to the Arduino
+        // Use data recieved method and a try catch block for any possible exception.
+        private void Init()
+        {
+            try
+            {
+                myPort = new SerialPort();
+                myPort.BaudRate = 9600;
+                myPort.PortName = "COM4"; // modify to your local COM
+                myPort.Parity = Parity.None;
+                myPort.StopBits = StopBits.One;
+                myPort.DataBits = 8;
+                myPort.Handshake = Handshake.None;
+                myPort.RtsEnable = true;
+                myPort.Open();
+                myPort.DataReceived += myPort_DataReceived;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Not connected");
+            }
+        }
+        // Serial port data received method which reads the data from the Arduino as string line.
+        // Then uses the line recieved method which converts the raw data in combination with the line received event.
+        private void myPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string line = myPort.ReadLine(); // read data from Arduino
+            BeginInvoke(new LineReceivedEvent(LineReceived), line);
+        }
+
+        private delegate void LineReceivedEvent(string line);
+
+        // convert raw data into correct format
+        private void LineReceived(string line)
+        {
+            OutsideTempBox.Clear();
+            InsideHumidityBox.Clear(); // clear text boxes
+            InsideTempBox.Clear();
+            // Split the data for each appropraite text box
+            String[] dataHumTemp = line.Split(',');
+            OutsideTempBox.Text = dataHumTemp[0];
+            InsideHumidityBox.Text = dataHumTemp[1];
+            InsideTempBox.Text = dataHumTemp[2];
+        }
+        // Click button method for turning the red LED light on and off.
+        private void RedLEDButton_Click(object sender, EventArgs e)
+        {
+            if (redLightStatus == false)
+            {
+                myPort.WriteLine("100");
+                redLightStatus = true;
+            }
+            else
+            {
+                myPort.WriteLine("101");
+                redLightStatus = false;
+            }
+        }
+        // Method for setting max temperature after which the buzzer will sound like an alarm.
+        private void TempMaxButton_Click(object sender, EventArgs e)
+        {
+            
+            myPort.WriteLine(TempComboBox.Text);
+
+        }
+    }
+}
+
